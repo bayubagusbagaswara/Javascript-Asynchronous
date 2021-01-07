7a20b1626ae557967808a9bd06446332b6f92313
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2021-01-07 19:58:27 +0700

    Membuat AJAX dan untuk mendapatkan data dari backend tidak bisa dilakukan secara synchronous

--- 3 AJAX/script.js
+++ 3 AJAX/script.js
@@ -4,20 +4,32 @@ function getProductsUrl(keyword) {
 
 function getProducts(keyword) {
   // Code AJAX Here!
+
+  // buat AJAX nya
   const ajax = new XMLHttpRequest();
+  // ambil dari URL nya
+  const url = getProductsUrl(keyword);
   ajax.open("GET", url);
+  // panggil AJAX nya
   ajax.send();
+
+  // untuk mendapatkan respon nya -> String JSON nya
+  // tidak bisa dilakukan secara synchronous
+  // const response = JSON.parse(ajax.responseText);
 }
 
+// Menghapus semua data product yang ada di list
 function clearProducts() {
   const productUl = document.getElementById("products");
   productUl.textContent = "";
 }
 
+// Menampilkan data-data dari respon backend
 function displayProducts(data) {
   data.data.products.forEach((product) => displayProduct(product));
 }
 
+// Menampilkan satu-satu data product kedalam list
 function displayProduct(product) {
   const productLi = document.createElement("li");
   productLi.textContent = product.name;
